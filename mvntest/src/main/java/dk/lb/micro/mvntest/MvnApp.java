53b03a006e0dda1a11373715cb84426f0908383d
Anders Kristian Andersen <aka@myself.com>
2017-11-12 21:56:47 +0100

    tilføjet mvn test app

---
+++ mvntest/src/main/java/dk/lb/micro/mvntest/MvnApp.java
@@ -0,0 +1,10 @@
+package dk.lb.micro.mvntest;
+
+import java.util.Arrays;
+
+public class MvnApp {
+
+    public static void main(String[] args){
+        System.out.println("Hej "+ Arrays.toString(args));
+    }
+}
